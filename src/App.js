be56b53b0de50f22b74ffa26053f3d82344f0366
Alexander Eriksson <aleer778@student.liu.se>
2021-06-29 21:56:04 +0200

    Added the feature to run scripts using path (bash)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+import TabsContent from "./components/TabsContent";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+            <div className="box">
+                <div className="row header">
+                    <section className="hero">
+                        <div className="hero-body">
+                            <div className="container has-text-centered">
+                                <p className="title">AFK Arena Helper</p>
+                                <p className="subtitle"><span role="img" aria-label="Thunderbolt">⚡</span> Electron App</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+
+                <div className="row content">
+                    <div id="tabs-with-content" style={{ height: "100%" }} >
+                        <TabsContent></TabsContent>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
